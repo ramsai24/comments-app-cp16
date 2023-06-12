9bb47258d76e23f547969a633f829794b9b99127
ramsai24 <jnrsai24@gmail.com>
2023-06-12 14:21:46 +0000

    all test cases passed but css is pending

--- src/components/CommentItem/index.js
+++ src/components/CommentItem/index.js
@@ -1,1 +1,65 @@
 // Write your code here
+import './index.css'
+
+const CommentItem = props => {
+  const {commentItem, deleteItem, isActiveLike} = props
+  const {id, name, comment, time, isActive} = commentItem
+
+  const deleteId = () => {
+    deleteItem(id)
+  }
+
+  const onLikeStatus = () => {
+    isActiveLike(id)
+  }
+
+  const likeIsActive = isActive ? (
+    <div>
+      <img
+        id="like"
+        src="https://assets.ccbp.in/frontend/react-js/comments-app/liked-img.png"
+        alt="like"
+      />
+      <button type="button" onClick={onLikeStatus}>
+        <label htmlFor="unLike">like</label>
+      </button>
+    </div>
+  ) : (
+    <div>
+      <img
+        id="like"
+        src="https://assets.ccbp.in/frontend/react-js/comments-app/like-img.png"
+        alt="like"
+      />
+      <button type="button" onClick={onLikeStatus}>
+        <label htmlFor="unLike">like</label>
+      </button>
+    </div>
+  )
+
+  return (
+    <li>
+      <div>
+        <p>{name[0]}</p>
+        <div>
+          <div>
+            <h1>{name}</h1>
+            <p>{time}</p>
+          </div>
+          <p>{comment}</p>
+        </div>
+      </div>
+      <div>
+        {likeIsActive}
+        <button data-testid="delete" type="button" onClick={deleteId}>
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/comments-app/delete-img.png"
+            alt="delete"
+          />
+        </button>
+      </div>
+    </li>
+  )
+}
+
+export default CommentItem
--- src/components/Comments/index.js
+++ src/components/Comments/index.js
@@ -1,11 +1,122 @@
-const initialContainerBackgroundClassNames = [
-  'amber',
-  'blue',
-  'orange',
-  'emerald',
-  'teal',
-  'red',
-  'light-blue',
-]
+import {Component} from 'react'
+import {formatDistanceToNow} from 'date-fns'
+import {v4 as uuidv4} from 'uuid'
+import './index.css'
+import CommentItem from '../CommentItem'
+
+// const initialContainerBackgroundClassNames = [
+//   'amber',
+//   'blue',
+//   'orange',
+//   'emerald',
+//   'teal',
+//   'red',
+//   'light-blue',
+// ]
 
 // Write your code here
+
+class Comments extends Component {
+  state = {name: '', comment: '', commentsList: []}
+
+  addCommentbtn = event => {
+    event.preventDefault()
+    const {name, comment} = this.state
+    console.log(name, comment)
+    const newCommment = {
+      id: uuidv4(),
+      name,
+      comment,
+      isActive: false,
+      time: formatDistanceToNow(new Date()),
+    }
+    this.setState(prevState => ({
+      commentsList: [...prevState.commentsList, newCommment],
+      name: '',
+      comment: '',
+    }))
+  }
+
+  isActiveLiked = id => {
+    console.log(id)
+    this.setState(prev => ({
+      commentsList: prev.commentsList.map(each => {
+        if (id === each.id) {
+          return {...each, isActive: !each.isActive}
+        }
+        return each
+      }),
+    }))
+  }
+
+  onDeleteItem = id => {
+    const {commentsList} = this.state
+    console.log(id)
+    const newCommentsList = commentsList.filter(each => id !== each.id)
+    this.setState({commentsList: newCommentsList})
+  }
+
+  inputElData = event => {
+    this.setState({name: event.target.value})
+  }
+
+  textAreaEl = event => {
+    this.setState({comment: event.target.value})
+  }
+
+  render() {
+    const {name, comment, commentsList} = this.state
+    console.log(commentsList)
+
+    return (
+      <div>
+        <div>
+          <h1>Comments</h1>
+          <p>Say something about 4.0 Technologies</p>
+          <form className="top-section">
+            <input
+              placeholder="Your Name"
+              type="text"
+              value={name}
+              onChange={this.inputElData}
+            />
+            <br />
+            <textarea
+              type="text"
+              value={comment}
+              cols="10"
+              rows="10"
+              onChange={this.textAreaEl}
+              placeholder="Your Comment"
+            >
+              {' '}
+            </textarea>
+            <button type="submit" onClick={this.addCommentbtn}>
+              Add Comment
+            </button>
+          </form>
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/comments-app/comments-img.png"
+            alt="comments"
+          />
+        </div>
+        <div className="bottom-section">
+          <span id="commentsCount">{commentsList.length}</span>
+          <label htmlFor="commentsCount">Comments</label>
+          <ul>
+            {commentsList.map(each => (
+              <CommentItem
+                key={each.id}
+                commentItem={each}
+                deleteItem={this.onDeleteItem}
+                isActiveLike={this.isActiveLiked}
+              />
+            ))}
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Comments
